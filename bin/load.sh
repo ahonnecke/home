0549206120694495374162a6077a56b5ece2e85f
Ashton Honnecke <ashton@havenly.com>
2016-01-30 12:02:44 -0700

    Should become redundant soon

--- bin/load.sh
+++ bin/load.sh
@@ -4,5 +4,5 @@ echo Loading
 #mysql -u root < /tmp/production.sql
 pv /tmp/production.sql | mysql -u root
 mysql -u root havenly_app < /Users/ahonnecke/sql/scrub_personal_info.sql
-#mysql -u root havenly_app < /Users/ahonnecke/sql/add_stamps.sql
+#mysql -u root havenly_app < /Users/ahonnecke/sql/change_stamps.sql
 echo Loaded
