e8b7a6695023c145a48009cc27845409f08bca57
Ashton Honnecke <ashton@havenly.com>
2016-05-08 10:10:48 -0600

    script to fix up domain model
    after composer install

--- bin/add-domain-remotes.sh
+++ bin/add-domain-remotes.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cd ~/www/havenly/src/havenly/Vendor/havenly/domain-model/
 git remote add ahonnecke git@github.com:ahonnecke/DomainModel.git
-git remote add wshafer git@github.com:wshafer/DomainModelg.git
+git remote add wshafer git@github.com:wshafer/DomainModel.git
+git remote add upstream git@github.com:havenly/DomainModel.git
+
 git fetch --all
