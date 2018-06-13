d58dc2f40035375a5025838ac82a86a5e54f2512
Ashton Honnecke <ashton@digitalassetsdata.com>
2018-06-13 11:57:55 -0600

    I think that chrome-driver is the good one

---
+++ bin/update-bionic-tag.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import json
+import re
+
+import requests
+from trepan.api import debug
+
+import git
+
+response = requests.get('https://registry.hub.docker.com/v1/repositories/ubuntu/tags')
+json_data = json.loads(response.text)
+
+latest = json_data.filter(lambda x: "bionic")
+
+debug()
+
+print(f'Found chrome version {chrome_version}')
+
+web_repo = git.Repo("/Users/ahonnecke/Code/repos/web-batch/")
+git = web_repo.git
+circlefile = "/Users/ahonnecke/Code/repos/web-batch/docker/Dockerfile.test"
+
+for remote in web_repo.remotes:
+    remote.fetch()
+
+new_branch = f'chrome-version-{chrome_version}'
+
+git.reset('--hard', 'upstream/master')
+git.checkout('upstream/master')
+try:
+    git.branch('-D', new_branch)
+except:
+    pass
+git.checkout('-b', new_branch, 'upstream/master')
+
+with open(circlefile, 'r') as reader:
+    content = reader.read()
+    content_new = re.sub(
+        'ENV CHROME_DRIVER_VERSION .*',
+        r'ENV CHROME_DRIVER_VERSION ' + str(chrome_version),
+        content,
+        flags=re.M
+    )
+
+    content_newer = re.sub(
+        'ADD http://chromedriver.storage.googleapis.com/.*/chromedriver_linux64.zip ',
+        r'ADD http://chromedriver.storage.googleapis.com/' + str(chrome_version) + '/chromedriver_linux64.zip ',
+        content_new,
+        flags=re.M
+    )
+
+with open(circlefile, "w") as writer:
+    writer.write(content_newer)
+    writer.close()
+
+changedFiles = [item.a_path for item in web_repo.index.diff(None)]
+if changedFiles:
+    web_repo.index.add(changedFiles)
+    git.commit('-m', f'Updating chrome version to {chrome_version}')
+    git.push('-v', 'origin', new_branch)
--- bin/update-ccxt-version.py
+++ bin/update-ccxt-version.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
 import os
+import time
 from subprocess import call
 
 import git
+# @todo use this library to create a PR
+from github import Github
 
 web_repo = git.Repo("/Users/ahonnecke/Code/repos/web-batch/")
 git = web_repo.git
@@ -14,7 +17,7 @@ for remote in web_repo.remotes:
 git.checkout('master')
 git.reset('--hard', 'upstream/master')
 
-os.chdir("/Users/ahonnecke/Code/repos/web/server")
+os.chdir("/Users/ahonnecke/Code/repos/web-batch/server")
 
 # couldn't find a good library for pipenv
 call(['pipenv', 'update', 'ccxt'])
@@ -26,7 +29,8 @@ print(changedFiles)
 if 'Pipfile.lock' in changedFiles:
     print("Pipfile.lock has changed")
     # @todo: add the version to the branch name
-    new_branch = f'ccxt-update'
+    epoch = time.time()
+    new_branch = f'ccxt-update{epoch}'
 
     try:
         git.branch('-D', new_branch)
@@ -38,3 +42,8 @@ if 'Pipfile.lock' in changedFiles:
     web_repo.index.add(['Pipfile.lock'])
     git.commit('-m', f'Updating ccxt version')
     git.push('-v', 'origin', new_branch)
+
+    # g = Github("ashton@pixelstub.com", "eat-sleep-code1")
+
+    # for repo in g.get_user().get_repos():
+    #     print(repo.name)
