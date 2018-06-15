47ef85bed05208f219cc281265d5b24d210fb7e9
Ashton Honnecke <ashton@digitalassetsdata.com>
2018-06-15 14:36:54 -0600

    some stuff

--- bin/warm-ci-cache.py
+++ bin/warm-ci-cache.py
@@ -4,24 +4,80 @@ from time import sleep
 
 import git
 
-web_repo = git.Repo("/Users/ahonnecke/Code/repos/web/")
+
+def ask_user(question):
+    check = str(raw_input(str(question) + " ? (Y/N): ")).lower().strip()
+    try:
+        if check[0] == 'y':
+            return True
+        elif check[0] == 'n':
+            return False
+        else:
+            print('Invalid Input')
+            return ask_user()
+    except Exception as error:
+        print("Please enter valid inputs")
+        print(error)
+        return ask_user(question)
+
+
+if ask_user("Is it OK to crush the CI pipeline? (this will make it unusable for a while)"):
+    print("Yeah! let's *DO THIS* (where 'this' is piss off devs)")
+    delay = 10
+else:
+    print("OK Slow and steady wins the race... or some such bullshit")
+    delay = 600 >
+
+
+web_repo = git.Repo("/Users/ahonnecke/Code/repos/web-batch/")
 
 for remote in web_repo.remotes:
     remote.fetch()
 
+git.checkout('master')
+git.reset('--hard', 'upstream/master')
+
+os.chdir("/Users/ahonnecke/Code/repos/web-batch/server")
+
+g = Github(os.environ['SERVICEDADTOKEN'])
+
 branch = web_repo.active_branch
 print(f'Active Branch: {branch.name}')
 
-config_filename = "/Users/ahonnecke/Code/repos/web/.circleci/config.yml"
+cache_warmer_filename = "/Users/ahonnecke/Code/repos/web-batch/.cache-warmer"
+
+print("Pipfile.lock has changed")
+new_branch = f'warm-ci-cache'
+
+try:
+    git.checkout(new_branch)
+except:
+    git.checkout('-b', new_branch, 'upstream/master')
+    pass
+
+base = "master"
+head = f'ahonnecke:{new_branch}'
+print(f'Opening PR to merge "{head}" into "{base}"')
+web_repo = org.get_repo('web')
+web_repo.create_pull(
+    title=f'Warm up cache',
+    body="Scripted update to warm up the CI cache",
+    base=base,
+    head=head
+)
 
 for x in range(0, 20):
     print(f'Pushing {x}...')
-    with open(config_filename, "a") as config_file:
+    with open(cache_warmer_filename, "a") as cache_warmer_filename:
         config_file.write(f'# {x} units warmer\n')
         config_file.close()
 
+    web_repo.index.add([cache_warmer_filename])
+    git.commit('-m', f'warming cache')
+    git.push('-v', 'origin', new_branch)
+
     web_repo.index.add([config_filename])
     web_repo.git.commit('-m', 'warming circleCI')
-<    web_repo.remotes.origin.push(refspec=f'{branch}:{branch}')
-    print(f'Sleeping after {x}...')
-    sleep(600)
+    web_repo.remotes.origin.push(refspec=f'{branch}:{branch}')
+    print(f'Sleeping {delay} seconds after iteration {x}...')
+    sleep(delay)
