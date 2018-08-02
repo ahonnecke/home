a6e17508bcf0a78f583f323bc9dddcbbe225f8c5
Ashton Honnecke <ashton@digitalassetsdata.com>
2018-08-02 16:06:38 -0600

    Updating the hooks a bit

--- hooks/pre-commit.d/mypy.sh
+++ hooks/pre-commit.d/mypy.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
-FILES=$(git diff --cached --name-status | grep -v node_modules | awk '$1 != "D" {print $2}' | grep -E '[.]py$' | grep -v migrations)
-[ "$NOCHECK" != "" ] || [ "$FILES" = "" ] || pipenv run mypy --ignore-missing-imports --strict-optional $FILES || exit 1
+MERGE=$(git rev-parse -q --verify MERGE_HEAD)
+
+FILES=$(git diff --cached --name-status | \
+            grep -v node_modules | \
+            awk '$1 != "D" {print $2}' | \
+            grep -E '[.]py$' \
+            | grep -v migrations)
+
+if [ -n "$MERGE" ]; then
+    [ "$NOCHECK" != "" ] \
+        || [ "$FILES" = "" ] \
+        || pipenv run mypy --ignore-missing-imports --strict-optional $FILES \
+        || exit 1
+fi
