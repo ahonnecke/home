50cd5f444ba581395e21b47d26a37f94c9f96eb6
Ashton Honnecke <ashton@digitalassetsdata.com>
2018-08-10 10:33:12 -0600

    fix up isort

---
+++ bin/run-all-tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+unset CLIENT_KEY
+unset PLAID_CLIENT_ID
+unset PLAID_SECRET
+unset PLAID_PUBLIC_KEY
+unset LEDGER_API_HOST
+unset CMC_API_HOST
+unset EMAIL_PASSWORD
+cd /Users/ahonnecke/Code/repos/web
+DJANGO_SETTINGS_MODULE=dad.config.local
+ENVIRON=local
+./run-all-tests.sh
