36a00a17f1bf637eae2f87351b73c252ee17a710
Ashton Honnecke <ashton@digitalassetsdata.com>
2018-08-15 16:08:06 -0600

    catch some erros

--- bin/install-hooks.sh
+++ bin/install-hooks.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd .git/hooks/
-mkdir pre-commit.d
-rm pre-commit
+rm pre-commit || true
 wget https://raw.githubusercontent.com/ahonnecke/home/master/hooks/pre-commit
 
+mkdir -p pre-commit.d
 cd pre-commit.d
 wget https://raw.githubusercontent.com/ahonnecke/home/master/hooks/pre-commit.d/pylint.py
 wget https://raw.githubusercontent.com/ahonnecke/home/master/hooks/pre-commit.d/isort.py
