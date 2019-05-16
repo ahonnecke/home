9dae20c43f8b4f6df0b0635c4e5bcd5fc5d459dc
Ashton Honnecke <ahonneck@gmail.com>
2019-05-16 17:10:24 -0600

    profiler

--- bin/dev-tf-ts-tunnel.sh
+++ bin/dev-tf-ts-tunnel.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+JUST_TUNNEL=$1
+
 [[ "$TRACE" ]] && set -x
 set -eu -o pipefail
 
@@ -10,6 +12,10 @@ BASTION_HOST="dev"
 
 ~/bin/tunnel.sh "$LABEL" $LOCAL_PORT $DEST_HOST $DEST_PORT $BASTION_HOST
 
-sleep 1
-
-pgcli -h localhost -p 15438 -U dev-app postgres
+if [ -z $JUST_TUNNEL ]; then
+    echo "JUST_TUNNEL is unset, connecting";
+    sleep 1
+    pgcli -h localhost -p $LOCAL_PORT -U dev-app postgres
+else
+    echo "JUST_TUNNEL is set ($JUST_TUNNEL), exiting";
+fi
---
+++ bin/profile_psql_query.py
@@ -0,0 +1,57 @@
+import os
+import subprocess
+import time
+
+import psycopg2
+import pyperclip
+import requests
+
+PG_HOST = os.environ.get('TUNNEL_DEV_PG_HOST', 'localhost')
+PG_DB = os.environ.get('TUNNEL_DEV_PG_DB', 'postgres')
+PG_USER = os.environ.get('TUNNEL_DEV_PG_USER', 'dev-app')
+PG_PORT = os.environ.get('TUNNEL_DEV_PG_PORT')
+PG_PW = os.environ.get('TUNNEL_DEV_PG_PW')
+
+if __name__ == "__main__":
+    # subprocess.run(["dev-tf-ts-tunnel.sh", "no"])
+
+    con = psycopg2.connect(f'postgresql://{PG_USER}:{PG_PW}@{PG_HOST}:{PG_PORT}/{PG_DB}')
+    # con.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+    cursor = con.cursor()
+    query = input('Raw Query: ')
+
+
+    start = time.time()
+    row = cursor.execute(f"EXPLAIN {query}")
+    end = time.time()
+    elapsed = end - start
+
+    print(f'Bare EXPLAIN took {elapsed}')
+    if elapsed > 2:
+        print('EXPLAIN took too long, exiting')
+        exit(1)
+
+    print('Performing EXPLAIN ANALYZE')
+    start = time.time()
+    row = cursor.execute(f"EXPLAIN ANALYZE {query}")
+    end = time.time()
+    elapsed = end - start
+
+    print(f'EXPLAIN ANALYZE took {elapsed}')
+
+    results = [x[0] for x in cursor.fetchall()]
+    plan = "\n".join(results)
+
+    #print(plan)
+
+    url = 'https://explain.depesz.com/'
+    r = requests.post(url, data={'plan': plan})
+
+    print(r.status_code, r.reason)
+
+    location = (r.url)
+    pyperclip.copy(location)
+
+    print(location)
+
+    subprocess.run(["google-chrome", location])
