2358982f489c448be7b6433991ad2be899091e8c
Ashton Honnecke <ahonneck@gmail.com>
2019-09-06 13:20:46 -0600

    config working

---
+++ bin/ec2-to-ssh-config.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+
+import boto3
+import os
+from pathlib import Path
+from shutil import copyfile
+
+HOME = str(Path.home())
+# PROFILES = ["web", "docker", "terraform", "nodes", "ops"]
+PROFILES = ["web", "nodes"]
+HOSTS = os.path.join(HOME, ".aws/hosts")
+AWS_CONF = os.path.join(HOME, ".ssh/aws.conf")
+STATIC_CONF = os.path.join(HOME, ".ssh/static.conf")
+SSH_CONF = os.path.join(HOME, ".ssh/config")
+
+
+def main():
+    results = {}
+    hostmap = []
+
+    for profile in PROFILES:
+        print(f"Assuming {profile}")
+        session = boto3.Session(profile_name=profile)
+        client = session.client("ec2")
+
+        response = client.describe_instances()
+
+        for r in response["Reservations"]:
+            for i in r["Instances"]:
+                tags = i.get("Tags", [])
+                name = None
+                for tag in tags:
+                    if tag.get("Key") == "Name":
+                        name = tag.get("Value").lower().replace(" ", "-")
+
+                private = i.get("PrivateIpAddress")
+                public = i.get("PublicIpAddress")
+
+                if not name or not i.get("KeyName"):
+                    continue
+
+                if not public and not private:
+                    continue
+
+                if "bastion" in name:
+                    continue
+
+                results[name] = i
+
+        for name, tags in results.items():
+            private = tags.get("PrivateIpAddress")
+            public = tags.get("PublicIpAddress")
+            keyname = tags.get("KeyName")
+
+            # if "ashton" not in name:
+            #     continue
+            # else:
+            print(name)
+
+            if private:
+                hostmap.append({"name": name, "ip": private, "keyname": keyname})
+
+            # if public:
+            #     name = f"{name}-public"
+            #     hostmap.append({"name": name, "ip": public, "keyname": keyname})
+
+    # with open(HOSTS, "w") as hostsfile:
+    #     for host in hostmap:
+    #         ip = host.get("ip")
+    #         name = host.get("name")
+    #         hostline = f"{ip.ljust(16)}          {name}"
+    #         print(f" * {hostline}")
+    #         hostsfile.write(f"{hostline}\n")
+
+    copyfile(STATIC_CONF, SSH_CONF)
+
+    for host in hostmap:
+        ip = host.get("ip")
+        name = host.get("name")
+        keyname = host.get("keyname").replace("-bastion", "")
+
+        ssh_block = f"""
+# {name} {keyname}
+Host {name}
+   Hostname {ip}
+   StrictHostKeyChecking no
+   User ubuntu
+   IdentityFile ~/.ssh/{keyname}.pem
+   ProxyCommand ssh -W %h:%p {keyname}-bastion
+
+"""
+
+        # file.write(ssh_block)
+
+        print(ssh_block)
+
+        with open(SSH_CONF, "a") as final:
+            final.write(ssh_block)
+
+
+if __name__ == "__main__":
+    main()
