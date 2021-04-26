a82b9dec6ce350fa2c7b99ca60afe21042340e26
Ashton Honnecke <ahonnecke@pop-os.localdomain>
2021-04-26 12:33:09 -0600

    fre install changes
    Signed-off-by: Ashton Honnecke <ahonnecke@pop-os.localdomain>

--- bin/fresh_install_setup.sh
+++ bin/fresh_install_setup.sh
@@ -8,18 +8,23 @@ ln -s ~/src/fzf/ .fzf
 
 pip install --user pylint pytest flake8 mypy pre-commit isort black boto3 trepan3k
 
+sudo apt install -y build-essential automake texinfo libjpeg-dev libncurses5-dev
+sudo apt install -y libtiff5-dev libgif-dev libpng-dev libxpm-dev libgtk-3-dev libgnutls28-dev
+sudo apt install -y libjansson4 libjansson-dev git
+
 #pull emacs
-cd ~/src/emacs/
-./configure --with-mailutils --prefix="${HOME}/local" --with-json
+cd ~/src/
+git clone git@github.com:emacs-mirror/emacs.git emacs
+./autogen.sh
+./configure --prefix="${HOME}/local" --with-json --with-mailutils
 make install
 
 #pull a fresh version of prelude, reinstall stuff
 
-
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo groupadd docker
-sudo usermod -aG docker $USER
-sudo chmod 666 /var/run/docker.sock
-newgrp docker
+# sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# sudo groupadd docker
+# sudo usermod -aG docker $USER
+# sudo chmod 666 /var/run/docker.sock
+# newgrp docker
 
 sudo curl -L https://raw.githubusercontent.com/docker/compose/1.26.2/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
