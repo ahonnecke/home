ea9216c18720f739d02527e5a118375327b6b860
Ashton Von Honnecke <ashton@pixelstub.com>
2021-06-30 16:18:29 -0600

    add toolkit
    Signed-off-by: Ashton Von Honnecke <ashton@pixelstub.com>

--- bin/cirrus_branch_name.py
+++ bin/cirrus_branch_name.py
@@ -5,23 +5,26 @@ import os
 import subprocess
 
 import git
+import prompt_toolkit
 from jira import JIRA
+from prompt_toolkit import prompt
+from prompt_toolkit.completion import WordCompleter
 
 REPO_ROOT = "/home/ahonnecke/src"
 
 
-def get_args():
+def get_args(repo_dirs):
     parser = argparse.ArgumentParser(description="Perform terraform tasks")
 
-    repo_dirs = [x for x in os.listdir(REPO_ROOT)]
-
     parser.add_argument(
-        "repo", help="Repository to create banch ing", choices=repo_dirs
+        "--repo",
+        help="Repository to create banch in",
+        choices=repo_dirs,
+        required=False,
     )
 
     parser.add_argument(
-        "ticket",
-        help="Ticket to build branch name from",
+        "--ticket", help="Ticket to build branch name from", required=False
     )
 
     return parser.parse_args()
@@ -29,9 +32,19 @@ def get_args():
 
 if __name__ == "__main__":
 
+    auth_email = "ashton.honnecke@us.panasonic.com"
+
     jira = JIRA("https://cirrusv2x.atlassian.net", basic_auth=(auth_email, api_token))
 
-    args = get_args()
+    repo_dirs = [x for x in os.listdir(REPO_ROOT)]
+    args = get_args(repo_dirs)
+
+    if args.repo:
+        repo = args.repo
+    else:
+        html_completer = WordCompleter(repo_dirs)
+        repo = prompt("Choose repository: ", completer=html_completer)
+        print("Using repository: %s" % repo)
 
     ticket = args.ticket.upper()
     myissue = jira.issue(ticket)
@@ -46,12 +59,11 @@ if __name__ == "__main__":
 
     # We're assuming that the user has cded to the appropriate dir
 
-    os.chdir(REPO_ROOT + "/" + args.repo)
+    os.chdir(REPO_ROOT + "/" + repo)
     print(branch_name)
 
     # git.checkout('dev')
     # git.reset('--hard', 'upstream/dev')
-
     # git checkout -b feature/V2X-2226_add-the-following-curves-to-the-curve-database upstream/dev
 
     cmd = ["git", "checkout", "-b", branch_name]
